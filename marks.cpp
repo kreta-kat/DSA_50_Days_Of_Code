f05450d4cf22c0d1307c8e05354fb0e655a742df
kreta-kat <85033106+kreta-kat@users.noreply.github.com>
2021-08-10 15:10:01 +0530

    Create marks.cpp

---
+++ marks.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int marks[n]; //100;
+    for(i=0; ;i<n;i++)
+    {
+        cin>>marks[i];
+    }
+    for(int i=0; i<n;i++)
+    {
+    if(marks>100 || marks<0)
+    return -1;
+
+    if(marks[i] >= 90)
+    {
+        a
+    }
+    else if(marks[i]>=80 && marks[i<90)
+    {
+        b
+    }
+    else if(marks[i]>=70 && marks[i]<80)
+    {
+        c
+    }
+    else if(marks[i]>=30 && marks[i]<70)
+    {
+        d
+    }
+    else
+    {
+        f
+    }
+    }
+    
+    return 0;
+}
