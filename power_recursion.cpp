81af02af2b74b0cbc1d0a448deba463a9c823256
kreta-kat <85033106+kreta-kat@users.noreply.github.com>
2021-08-11 02:50:40 +0530

    Create power_recursion.cpp

---
+++ power_recursion.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int pow(int m,int n)
+{
+   
+   if(n == 0)
+    {
+        return 1;
+    }
+    else if(n%2 == 0)
+    {
+        return pow(m*m,n/2);
+    }
+    else
+    {
+        return m*pow(m*m, (n-1)/2);
+    }
+    
+}
+int main()
+{
+    int m=5,n=3;
+    
+    cout<<pow(m,n);
+}
